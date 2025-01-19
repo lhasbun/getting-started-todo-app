de8e4e76fe56c9c604a3889151e12efdcb0262c3
Luis Hasbun <41136478+lhasbun@users.noreply.github.com>
2025-01-18 20:41:51 -0600

    Update getGreeting

--- backend/src/routes/getGreeting.js
+++ backend/src/routes/getGreeting.js
@@ -6,6 +6,6 @@ const GREETINGS = [
 
 module.exports = async (req, res) => {
     res.send({
-        greeting: GREETINGS[ Math.floor( Math.random() * GREETINGS.length )],
+        greeting: GREETINGS[Math.floor( Math.random() * GREETINGS.length)],
     });
 };
